b0c1bdcb7788e83b8f52004aabbab7bd709ec1ef
Santiago Barrera <santiago.barrerae@gmail.com>
2023-02-23 00:20:05 -0500

    Integración con Kafka

--- src/messages/messages.service.ts
+++ src/messages/messages.service.ts
@@ -1,6 +1,7 @@
 //Libraries
 import { Injectable } from '@nestjs/common';
 import { randomUUID } from 'crypto';
+import { KafkaClient, Producer } from 'kafka-node';
 
 //DTOs
 import { NewMessageInput } from './dto/new-message.input';
@@ -10,10 +11,20 @@ import { Message } from './models/message.model';
 
 @Injectable()
 export class MessagesService {
+  private readonly client = new KafkaClient({ kafkaHost: '127.0.0.1:9091' });
+  private readonly producer = new Producer(this.client);
+
   async create(data: NewMessageInput): Promise<Message> {
     const newMessage = new Message();
     newMessage.id = randomUUID();
     newMessage.message = data.message;
+
+    newMessage.message &&
+      this.producer.send(
+        [{ topic: 'messages.basic', messages: newMessage.message }],
+        (err) => err && console.error(err),
+      );
+
     return newMessage;
   }
 
